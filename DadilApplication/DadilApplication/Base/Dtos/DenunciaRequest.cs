90532846ac4741decd9d42e3d7e2dab7494fdfc5
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2025-05-18 19:02:05 -0300

    ajustnado denuncia post e put para usar DTO

---
+++ DadilApplication/DadilApplication/Base/Dtos/DenunciaRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace DadilApplication.Base.DTOs
+{
+    public record DenunciaRequest(
+     string Descricao,
+     string? Localizacao,
+     string? FotoUrl,
+     int Status,
+     DateTime DataCriacao,
+     int UsuarioId
+ );
+}
